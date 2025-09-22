a602c6dfd940e3aa7e482b60d55bb3a9817ce926
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-09-23 00:51:51 +0300

    trying to get a cube on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include "../include/glad/glad.h"
 #include <GLFW/glfw3.h>
+#include <cstddef>
 #include <iostream>
+
+#include "../include/glm/glm.hpp"
+#include "../include/glm/gtc/matrix_transform.hpp"
+#include "../include/glm/gtc/type_ptr.hpp"
 
 #include <vector>
 #include <cmath>
@@ -10,7 +15,8 @@
 #include "../include/LoadShaders.h"
 #include "../include/initWindow.h"
 #include "../include/loadTexture.h"
-#include "../include/Mat4.h" 
+//#include "../include/Mat4.h"
+
 using namespace std;
 
 #define SCREEN_WIDTH 700
@@ -20,9 +26,9 @@ using namespace std;
 int g_width = SCREEN_WIDTH;
 int g_height = SCREEN_HEIGHT;
 
-GLuint quadVAO = 0;
-GLuint quadVBO = 0;
-GLuint quadEBO = 0;
+GLuint cubeVAO = 0;
+GLuint cubeVBO = 0;
+GLuint cubeEBO = 0;
 GLuint shaderProgram = 0;
 
 vector<GLuint> textures;
@@ -32,63 +38,94 @@ vector<const char *> textureNames = {
   "../res/ceiling.jpg"
 };
 
-// quad vertices
-const GLuint NumVertices = 4;
-// (x, y)-position, (u, v)-texture coords 
-static const GLfloat vertices[NumVertices * 4] = {
-  -0.5f, -0.5f,      0.0f, 0.0f,
-  +0.5f, -0.5f,      1.0f, 0.0f,
-  +0.5f, +0.5f,      1.0f, 1.0f,
-  -0.5f, +0.5f,      0.0f, 1.0f
+// cube vertices with texture coordinates
+const GLuint NumVertices = 36;
+
+// (x, y, z)-position, (u, v)-texture coordinates 
+static const GLfloat cubeVertices[NumVertices * 5] = {
+  //front
+  -0.5f, -0.5f, 0.5f,   0.0f, 0.0f,
+   0.5f, -0.5f, 0.5f,   1.0f, 0.0f,
+   0.5f,  0.5f, 0.5f,   1.0f, 1.0f,
+   0.5f,  0.5f, 0.5f,   1.0f, 1.0f,
+  -0.5f,  0.5f, 0.5f,   0.0f, 1.0f,
+  -0.5f, -0.5f, 0.5f,   0.0f, 0.0f,
+
+  //back
+  -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+   0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+   0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+   0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+  -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+  -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+  //left
+  -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+  -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+  -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+  -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+  -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+  -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+  //right
+  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+  0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+  0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+  0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+  //bottom
+  -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+   0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+   0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+   0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+  -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+  -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+  //top
+  
+  -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+   0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+   0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+   0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+  -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+  -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
 };
 
-static const GLuint indices[] = {
-    0, 1, 2, // first tri
-    2, 3, 0  // second tri
-};
-
-
-struct QuadInstance {
-  float x, y, z;
-  float scaleX, scaleY, scaleZ;
-  float rotation;
+struct CubeInstance {
+  glm::vec3 position;
+  glm::vec3 scale;
+  glm::vec3 rotation;
   int textureIndex;
 };
 
 // define quads
-vector<QuadInstance> quadInstances = {
-  // x,   y,    z,    scaleX,  scaleY,  scaleZ,  rotation,    textureIndex
-  {0.0f,  0.5f, 0.0f, 1.0f,    1.0f,    1.0f,     0.0f,        0}, 
-  {0.5f,  0.5f, 0.0f, 1.2f,    0.8f,    1.0f,     0.3f,        1},
-  {0.0f, -1.2f, 0.0f, 0.8f,    0.8f,    1.0f,    -0.5f,        2},
-  {0.0f,  1.8f, 0.0f, 1.5f,    0.6f,    1.0f,     0.0f,        0},
-  {0.8f,  0.3f, 0.0f, 0.6f,    1.4f,    1.0f,     1.0f,        1}
+vector<CubeInstance> cubeInstances = {
+  //position,           scale,               rotation,           textureIndex
+  {{0.0f,  0.5f, 0.0f}, {1.0f, 1.0f, 1.0f},  {0.0f, 0.0f, 0.0f},  0}
 };
 
 void setupQuad()
 {
   //gen and bind vao and vbo
-  glGenVertexArrays(1, &quadVAO);
-  glGenBuffers(1, &quadVBO);
-  glGenBuffers(1, &quadEBO);
+  glGenVertexArrays(1, &cubeVAO);
+  glGenBuffers(1, &cubeVBO);
+  glGenBuffers(1, &cubeEBO);
 
   // bind vao first
-  glBindVertexArray(quadVAO);
+  glBindVertexArray(cubeVAO);
 
   //bind and fill vbo
-  glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
 
-  //indices buffer
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quadEBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-  
   //setup vertex attrib
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)0);
   glEnableVertexAttribArray(0);
 
   // texture coord loc=1
-  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
   glEnableVertexAttribArray(1);
   
   glBindVertexArray(0);
@@ -96,7 +133,7 @@ void setupQuad()
 
 void setupShaders()
 {
-  auto shader = parseShader("../res/transformed.glsl");
+  auto shader = parseShader("../res/cube.glsl");
   shaderProgram = createShader(shader.vertexShader, shader.fragmentShader);
 }
 
@@ -117,38 +154,80 @@ void display()
   //clear screen
   static const float darkGray[] = {0.2f, 0.2f, 0.2f, 1.0f};
   glClearColor(darkGray[0], darkGray[1], darkGray[2], darkGray[3]);
-  glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   //use shader program
   glUseProgram(shaderProgram);
-  
-  //gete uniform locations
-  GLint modelLocation = glGetUniformLocation(shaderProgram, "u_model");  
+
+  //create view matrix
+  glm::mat4 view = glm::translate(glm::mat4(1.0f),
+				  glm::vec3(0.0f, -1.0f, -8.0f));
+
+  //create perspective projection matrix
+  float aspectRatio = (float)g_width / (float)g_height;
+  glm::mat4 projection = glm::perspective(glm::radians(45.0f),
+					  aspectRatio,
+					  0.1f,
+					  100.0f);
+
+  //get uniform locations
+  GLint modelLocation = glGetUniformLocation(shaderProgram, "u_model");
+  GLint viewLocation = glGetUniformLocation(shaderProgram, "u_view");
+  GLint projectionLocation = glGetUniformLocation(shaderProgram, "u_projection");
   GLint textureLocation = glGetUniformLocation(shaderProgram, "u_texture");
 
-  //bind vao and draw
-  glBindVertexArray(quadVAO);
+  //send view and projection matrices
+  glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
+  glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(projection));
 
-  //render each quad instance
-  for (const auto& quad : quadInstances) {
-    Mat4 translation = Mat4::translate(quad.x, quad.y, quad.z);
-    Mat4 rotation = Mat4::rotateZ(quad.rotation);
-    Mat4 scaling =  Mat4::scale(quad.scaleX, quad.scaleY, quad.scaleZ);
+  glBindVertexArray(cubeVAO);
 
-    //combine transformations
-    Mat4 model = translation * rotation * scaling;
+  //render cubes
+  static float time = 0.0f;
+  time += 0.016; //60 FPS
 
-    //send matrix to shader
-    glUniformMatrix4fv(modelLocation, 1, GL_FALSE, model.m);
+  for (size_t i = 0; i < cubeInstances.size(); i++)
+    {
+      auto cube = cubeInstances[i];
 
-    //bind texture
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, textures[quad.textureIndex]);
-    glUniform1i(textureLocation, 0);
+      if (i == 1)
+	{
+	  cube.rotation.y += time * 0.5;
+	}
+      if (i == 4)
+	{
+	  cube.rotation.x += time * 0.8f;
+	  cube.rotation.z += time * 1.2f;
+	}
 
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-  }
+      //create transformation matrix
+      glm::mat4 model = glm::mat4(1.0f);
 
+      //apply transformation translate, rotate, scale
+      model = glm::translate(model, cube.position);
+      model = glm::rotate(model,
+			  cube.rotation.y,
+			  glm::vec3(0.0f, 1.0f, 0.0f));
+      model = glm::rotate(model,
+			  cube.rotation.x,
+			  glm::vec3(1.0f, 0.0f, 0.0f));
+      model = glm::rotate(model,
+			  cube.rotation.z,
+			  glm::vec3(0.0f, 0.0f, 1.0f));
+      model = glm::scale(model, cube.scale);
+
+      //send model to shader
+      glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+
+      //bind texture
+      glActiveTexture(GL_TEXTURE0);
+      glBindTexture(GL_TEXTURE_2D, textures[cube.textureIndex]);
+      glUniform1i(textureLocation, 0);
+
+      glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+			
+    }
+ 
   glBindVertexArray(0);
 }
 
@@ -162,20 +241,15 @@ void processInput(GLFWwindow* window)
 
 void cleanup()
 {
-  if (quadVBO != 0)
+  if (cubeVBO != 0)
     {
-      glDeleteBuffers(1, &quadVBO);
-      quadVBO = 0;
+      glDeleteBuffers(1, &cubeVBO);
+      cubeVBO = 0;
     }
-  if (quadEBO != 0)
+  if (cubeVAO != 0)
     {
-      glDeleteBuffers(1, &quadEBO);
-      quadEBO = 0;
-    }
-  if (quadVAO != 0)
-    {
-      glDeleteVertexArrays(1, &quadVAO);
-      quadVAO = 0;
+      glDeleteVertexArrays(1, &cubeVAO);
+      cubeVAO = 0;
     }
   textures.clear();
   if (shaderProgram != 0)
