8cf5f6994d305fd90aacb5ad3cf99eecf0790634
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2025-09-28 11:07:51 +0300

    camera class

---
+++ include/Camera.h
@@ -0,0 +1,44 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+class Camera {
+ public:
+  Camera(glm::vec3 position = glm::vec3(3.0f, 0.0f, 3.0f),
+	 glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
+	 float yaw = 90.0f,
+	 float pitch = 0.0f);
+
+  glm::mat4 getViewMatrix();
+
+  //process keyboard
+  void processKeyboard(int direction, float deltaTime);
+
+  //process mouse
+  void processMouseMovement(float xoffset,
+			    float yoffset,
+			    bool constrainPitch = true);
+  //gets
+  glm::vec3 getPosition() const { return position; }
+  glm::vec3 getFront() const { return front; }
+  glm::vec3 getUp() const { return up; }
+  glm::vec3 getRight() const { return right; }
+  
+ private:
+  glm::vec3 position;
+  glm::vec3 front;
+  glm::vec3 up;
+  glm::vec3 right;
+
+  float yaw;
+  float pitch;
+
+  float movementSpeed;
+  float mouseSensitivity;
+
+  void updateCameraVectors();
+};
+
+#endif
